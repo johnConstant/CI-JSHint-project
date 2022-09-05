eee97c970fb6d7959bb972137755e7bbc2b4709e
John Constant <johnconstantis@gmail.com>
2022-09-05 20:49:49 +0000

    Add postForm function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const API_URL = 'https://ci-jshint.herokuapp.com/api';
 const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
 
 document.getElementById('status').addEventListener('click', (e) => getStatus(e));
+document.getElementById('submit').addEventListener('click', (e) => postForm(e));
 
 const getStatus = async () => {
     const res = await fetch(`${API_URL}?api_key=${API_KEY}`)
@@ -18,4 +19,21 @@ const displayStatus = (data) => {
     document.getElementById('resultsModalTitle').innerText = 'API Key Status';
     document.getElementById('results-content').innerText = `Your API key is due to expire on the ${data}`;
     resultsModal.show();
+}
+
+const postForm = async () => {
+    const form = new FormData(document.getElementById('checksform'));
+    const response = await fetch(API_URL, {
+                        method: "POST",
+                        headers: {
+                                    "Authorization": API_KEY,
+                                 },
+                        body: form
+                        })
+        const data = await response.json();
+        if(response.ok){
+            console.log(data);
+        } else {
+            throw new Error(data.error)
+        }               
 }
